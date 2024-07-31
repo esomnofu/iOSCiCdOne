b6b4be5ef6b7ed20e237f92f97f9ac1decdc457e
Christian Esomnofu <christian.esomnofu@capricorndigi.com>
2024-07-31 18:38:42 +0100

    Initial Commit

---
+++ CiCDOne/CiCDOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiCDOneApp.swift
+//  CiCDOne
+//
+//  Created by MAC on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CiCDOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
