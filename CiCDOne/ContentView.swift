d168a1d944302f3bc7de226373533e7b5741ab06
Christian Esomnofu <christian.esomnofu@capricorndigi.com>
2024-08-08 01:57:43 +0100

    Ref: added mercury

--- CiCDOne/ContentView.swift
+++ CiCDOne/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("Welcome Onboard, Mercury!")
             Text("Welcome Onboard, Jupiter!")
             Text("Welcome Onboard, Neptune!")
             Text("Welcome Onboard, Saturn!")
